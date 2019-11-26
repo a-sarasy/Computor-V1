dad04eb7eafaa47bd7bedf5062696514fac55246
Alexandre SARASY <asarasy@e1r10p5.42.fr>
2019-11-26 18:13:20 +0100

    Parser fonctionnel (petites choses a ameliorer) et output de la forme reduite 	modified:   computorv1.py

--- computorv1.py
+++ computorv1.py
@@ -2,25 +2,36 @@ import sys
 
 print(sys.argv[1])
 baseForm = sys.argv[1].split(' ')
-sign = 1
+sign = '+'
 stockNumber = []
 postEqual = 1
 print(baseForm)
 for x in baseForm:
     if x.startswith('X'):
         prime = int(x[2:])
-        stockNumber.append(prime, nbr)
-    elif x == '+' or x == '-':
+        stockNumber.append([prime, nbr])
+    elif x == '+' or x == '-' or x == '*':
         sign = x
     elif x == '=':
         postEqual = -1
-   
+        sign = '+' 
     else:
+        x = sign + x
         if '.' in x:
-            nbr = float(x) * postEqual * sign
+            nbr = float(x) * postEqual
         else:
-            nbr = int(x) * postEqual * sign
+            nbr = int(x) * postEqual
 print(stockNumber)
+reducedList = []
+for term in stockNumber :
+    while term[0] >= len(reducedList):
+        reducedList.append(0)
+    reducedList[term[0]] += term[1]
+print(reducedList)
 
-
-
+outReduc = "Reduced form:"
+for x in range(len(reducedList)):   
+    sign = ' + ' if reducedList[x] > 0 else ' - '
+    if x == 0 : sign = " "
+    outReduc += sign + str(abs(reducedList[x])) + " * X^" + str(x)
+print(outReduc + " = 0")
