63de5d3132e0f2a977bbd149e3c4a7fa369359bb
Alexandre SARASY <asarasy@e1r10p5.42.fr>
2020-02-04 15:59:28 +0100

    new parsing

--- computorv1.py
+++ computorv1.py
@@ -1,56 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-
-def parsingEq(baseForm):
-#Parsing de l'entrée(String de forme 5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0)
-#Liste en sortie avec les valeurs ordonnées
-    sign = '+'
-    stockNumber = []
-    postEqual = 1
-
-    for x in baseForm:
-        if x.startswith('X'):
-            prime = int(x[2:])
-            stockNumber.append([prime, nbr])
-        elif x == '+' or x == '-' or x == '*':
-            sign = x
-        elif x == '=':
-            postEqual = -1
-            sign = '+' 
-        else:
-            x = sign + x
-            if '.' in x:
-                nbr = float(x) * postEqual
-            else:
-                nbr = int(x) * postEqual
-    reducedList = []
-    for term in stockNumber :
-        while term[0] >= len(reducedList):
-            reducedList.append(0)
-        reducedList[term[0]] += term[1]
-    while reducedList[-1] == 0:
-        reducedList = reducedList[:-1]
-
-    return(reducedList)
+from parser import parsingEquation
 
 def fraction(up, down, iUp = 0):
+    print('ol')
     if iUp != 0:
         if iUp/down > 0: output = str(up/down) + ' + ' + str(iUp/down) + 'i'
         else: output = str(up/down) + ' - ' + str(abs(iUp/down)) + 'i'
         print(output)
     elif '.' in str(up) or '.' in str(down):
         print(str((up)/(down)))
+    
     else:
         x = abs(up)  if abs(up) < abs(down) else abs(down)
+        print(x)
+
         while x > 0:
+            print(x)
             if abs(up) % x == 0 and abs(down) % x == 0:
                 up,down = up/x,down/x
                 if down == 1:
                     print(str(int(up)))
                 else:
-                    print (str(int(up/x)) + '/' + str(int(down/x)))
+                    print (str(int(up)) + '/' + str(int(down)))
                 break
             x -= 1
 
@@ -77,26 +50,25 @@ def secondPolynom(a, b, c):
         fraction(up,down)
         fraction(upBis,downBis)
 
-if(len(sys.argv) < 2):
-    print("usage : python computorv1.py \"polynomial expression\"")
-    exit()
-baseForm = sys.argv[1].split(' ')
-reducedList = parsingEq(baseForm)
-outReduc = "Reduced form:"
-for x in range(len(reducedList)):   
-    sign = ' + ' if reducedList[x] > 0 else ' - '
-    if x == 0 and reducedList[x] > 0: sign = " "
+def computorv1():
+    if(len(sys.argv) != 2):
+        print("usage : python computorv1.py \"polynomial expression\"")
+        exit()
+    reducedList = parsingEquation(sys.argv[1])
+    print(reducedList)
+    polyNbr = len(reducedList) - 1
+    print("Polynomial degree: " + str(polyNbr))
+    if polyNbr > 2:
+        print("The polynomial degree is stricly greater than 2, I can't solve.")
+        exit(0)
+    if polyNbr == 2:
+        secondPolynom(reducedList[2], reducedList[1], reducedList[0])
+    if polyNbr == 1:
+        print("The solution is : ")
+        print('ol')
+        fraction(-reducedList[0] , reducedList[1])
+    if polyNbr == 0:
+        print("No unknown : all numbers are solutions")
 
-    outReduc += sign + str(abs(reducedList[x])) + " * X^" + str(x)
-print(outReduc + " = 0")
-polyNbr = len(reducedList) - 1
-print("Polynomial degree: " + str(polyNbr))
-if polyNbr > 2:
-    print("The polynomial degree is stricly greater than 2, I can't solve.")
-    exit(0)
-if polyNbr == 2:
-    secondPolynom(reducedList[2], reducedList[1], reducedList[0])
-if polyNbr == 1:
-    print("The solution is : " + str(-reducedList[0] / reducedList[1]))
-if polyNbr == 0:
-    print("No unknown : all numbers are solutions")
+if __name__ == "__main__":
+    computorv1()
---
+++ parser.py
@@ -0,0 +1,67 @@
+import re
+import sys
+
+def errorParsing(error):
+    print(error)
+    exit(0)
+
+def delete_whitespace(to_purge):
+    purged = ""
+    for x in to_purge:
+        if not x == ' ':
+            purged += x
+    return purged
+
+def get_value(expressions,mult):
+    dict_return = {}
+    x = re.search("^([+-]?[0-9]+(?:\.[0-9]+)?)\*X\^([0-9]+)",expressions)
+    y = 0
+    while y < len(expressions):    
+        if x == None:
+            errorParsing("Expression incorrecte")
+        y += x.span()[1]
+        if x.group(2)in dict_return.keys():
+            dict_return[int(x.group(2))] += float(x.group(1)) * mult
+        else:
+            dict_return[int(x.group(2))] = float(x.group(1)) * mult
+        x = re.search("^([+-][0-9]+(?:\.[0-9]+)?)\*X\^([0-9]+)",expressions[y:])
+    return dict_return
+
+def fusionDict(dict1, dict2):
+    for x,y in dict2.items():
+        if x in dict1.keys():
+            dict1[x] += y
+        else:
+            dict1[x] = y
+    outReduc = "Reduced form:"     
+    for x in list(dict1):
+        if dict1[x] == 0:
+            dict1.pop(x)
+        elif dict1[x] % int(dict1[x]) == 0:
+            dict1[x] = int(dict1[x])
+    reducedList = []
+    for key in sorted(dict1):
+        while key >= len(reducedList):
+            reducedList.append(0)
+        reducedList[key] += dict1[key]
+        sign=' +'
+        if len(outReduc) == 13:
+            sign=''
+        if dict1[key] < 0:
+            sign=' -'
+        outReduc += "{} {} * X^{}".format(sign, abs(dict1[key]), key)
+    outReduc += ' = 0'
+    if len(reducedList) > 1:
+        print (outReduc)
+    return reducedList
+
+def parsingEquation(all_str):
+    cleaned_str = delete_whitespace(all_str)
+    splited = cleaned_str.split('=')
+    if len(splited) != 2:
+        errorParsing('Not correct input')
+    before_equal = splited[0]
+    after_equal = splited[1]
+    dict1 = get_value(before_equal, 1)
+    dict2 = get_value(after_equal, -1)
+    return fusionDict(dict1,dict2)
